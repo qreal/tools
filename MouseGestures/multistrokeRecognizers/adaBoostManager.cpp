b4f20fdf9d9054a8614a144d25efa32275ef8ac0
osechkina_masha <osechkina.masha@gmail.com>
2012-06-19 16:10:26 +0400

    make adaboost testing faster

--- MouseGestures/multistrokeRecognizers/adaBoostManager.cpp
+++ MouseGestures/multistrokeRecognizers/adaBoostManager.cpp
@@ -4,98 +4,91 @@
 #include "multistrokeGesturesManagers.h"
 #include "rectanglegesturesmanager.h"
 #include "nearestposgridgesturesmanager.h"
+#include "QDebug"
 
 AdaBoostManager::AdaBoostManager()
 {
-	mManagers.append(new RectangleGesturesManager());
-	mManagers.append(new NearestPosGridGesturesManager());
-	mManagers.append(new LevenshteinXYSortGesturesManager());
-	for(int i = 0; i < mManagers.length(); ++ i) {
-		mManagersWeights.append(0);
-	}
+    mRecognizers.append(new AbstractRecognizer(new RectangleGesturesManager()));
+    mRecognizers.append(new AbstractRecognizer(new NearestPosGridGesturesManager()));
+    //mRecognizers.append(new AbstractRecognizer(new LevenshteinXYSortGesturesManager()));
+    for(int i = 0; i < mRecognizers.length(); ++ i) {
+        mRecognizersWeights.append(0);
+    }
 }
 
 QString AdaBoostManager::recognizeObject(PathVector const & gesture)
 {
-	QList<AbstractRecognizer *> recognizers;
-	foreach(GesturesManager * manager, mManagers) {
-		recognizers.append(new AbstractRecognizer(manager, mObjects));
-	}
-	double maxProbability = 0;
-	QString resultantObject = mObjects[0].first;
-	foreach (Entity object, mObjects) {
-		double probability = 0;
-		for(int i = 0; i < recognizers.length(); ++ i) {
-			QString answer = recognizers[i]->recognizeObject(gesture);
-			if (answer == object.first) {
-				probability += mManagersWeights[i];
-			}
-		}
-		if (probability > maxProbability) {
-			maxProbability = probability;
-			resultantObject = object.first;
-		}
-	}
-	foreach(AbstractRecognizer * recognizer, recognizers) {
-		delete recognizer;
-	}
-	return resultantObject;
+    double maxProbability = 0;
+    QString resultantObject = mObjects[0].first;
+    foreach (Entity object, mObjects) {
+        double probability = 0;
+        for(int i = 0; i < mRecognizers.length(); ++ i) {
+            QString answer = mRecognizers[i]->recognizeObject(gesture);
+            if (answer == object.first) {
+                probability += mRecognizersWeights[i];
+            }
+        }
+        if (probability > maxProbability) {
+            maxProbability = probability;
+            resultantObject = object.first;
+        }
+    }
+    return resultantObject;
 }
 
 void AdaBoostManager::setIdealGestures(const QList<Entity> & objects)
 {
-	mObjects = objects;
+    mObjects = objects;
 }
 
 double AdaBoostManager::listSum(QList<double> list)
 {
-	double sum = 0;
-	foreach(double i, list) {
-		sum += i;
-	}
-	return sum;
+    double sum = 0;
+    foreach(double i, list) {
+        sum += i;
+    }
+    return sum;
 }
 
 void AdaBoostManager::learn(QList<Entity> const & trainingSet)
 {
-	QList<double> alphas; //weights for the classifiers
-	for (int i = 0; i < mManagers.length(); ++ i) {
-		alphas.append(0);
-	}
-	int n = trainingSet.length();
-	//Initialize the observation weights
-	QList<double> weights;
-	for(int i = 0; i < n; ++ i) {
-		weights.append(static_cast<double>(1) / n);
-	}
-	foreach (GesturesManager * manager, mManagers)
-	{
-		AbstractRecognizer * recognizer = new AbstractRecognizer(manager, mObjects);
-		//Calculate the error
-		double error = 0;
-		foreach(Entity sample, trainingSet) {
-			if (recognizer->recognizeObject(sample.second) != sample.first)
-				error += weights.at(trainingSet.indexOf(sample));
-		}
-		error = error / listSum(weights);
-		if (error > 0.5) {
-			continue;
-		}
-		//Compute the weight for the classifier
-		alphas[mManagers.indexOf(manager)] = log((1 - error) / error);
-		//Set the new weights for the training data
-		for(int i = 0; i < weights.length(); ++ i) {
-			Entity sample = trainingSet[i];
-			if (recognizer->recognizeObject(sample.second) != sample.first)
-				weights[i] = weights[i] * exp(alphas[mManagers.indexOf(manager)]);
-		}
-		//Re-normalize the weights for the training data
-		double weightsSum = listSum(weights);
-		for(int i = 0; i < weights.length(); ++ i) {
-			weights[i] = weights[i] / weightsSum;
-		}
-		delete recognizer;
-	}
+    QList<double> alphas; //weights for the classifiers
+    for (int i = 0; i < mRecognizers.length(); ++ i) {
+        alphas.append(0);
+    }
+    int n = trainingSet.length();
+    //Initialize the observation weights
+    QList<double> weights;
+    for(int i = 0; i < n; ++ i) {
+        weights.append(static_cast<double>(1) / n);
+    }
+    foreach (AbstractRecognizer * recognizer, mRecognizers)
+    {
+        recognizer->setIdealGestures(mObjects);
+        //Calculate the error
+        double error = 0;
+        foreach(Entity sample, trainingSet) {
+            if (recognizer->recognizeObject(sample.second) != sample.first)
+                error += weights.at(trainingSet.indexOf(sample));
+        }
+        error = error / listSum(weights);
+        if (error > 0.5) {
+            continue;
+        }
+        //Compute the weight for the classifier
+        alphas[mRecognizers.indexOf(recognizer)] = log((1 - error) / error);
+        //Set the new weights for the training data
+        for(int i = 0; i < weights.length(); ++ i) {
+            Entity sample = trainingSet[i];
+            if (recognizer->recognizeObject(sample.second) != sample.first)
+                weights[i] = weights[i] * exp(alphas[mRecognizers.indexOf(recognizer)]);
+        }
+        //Re-normalize the weights for the training data
+        double weightsSum = listSum(weights);
+        for(int i = 0; i < weights.length(); ++ i) {
+            weights[i] = weights[i] / weightsSum;
+        }
+    }
 
-	mManagersWeights = alphas;
+    mRecognizersWeights = alphas;
 }
--- MouseGestures/multistrokeRecognizers/adaBoostManager.h
+++ MouseGestures/multistrokeRecognizers/adaBoostManager.h
@@ -6,22 +6,22 @@
 class AdaBoostManager : public RecognizerInterface
 {
 public:
-	AdaBoostManager();
-	double getMaxDistance(QString const &);
-	bool isMultistroke();
-	double getDistance(QList<double *> const & key1, QList<double *> const & key2);
-	void setKey(PathVector const & path);
-	void setIdealGestures(const QList<Entity> & objects);
-	void learn(QList<Entity> const & trainingSet);
+    AdaBoostManager();
+    double getMaxDistance(QString const &);
+    bool isMultistroke();
+    double getDistance(QList<double *> const & key1, QList<double *> const & key2);
+    void setKey(PathVector const & path);
+    void setIdealGestures(const QList<Entity> & objects);
+    void learn(QList<Entity> const & trainingSet);
 
 private:
-	QString recognizeObject(PathVector const & gesture);
-	QString recognizeObject();
-	double listSum(QList<double> list);
-	QList<GesturesManager *> mManagers;
-	QList<double> mManagersWeights;
-	GesturesManager * mGesturesManager;
-	QList<Entity> mObjects;// Ideal gestures (name - path)
-	QString mLastGesture;
-	PathVector mGesture;
+    QString recognizeObject(PathVector const & gesture);
+    QString recognizeObject();
+    double listSum(QList<double> list);
+    QList<AbstractRecognizer *> mRecognizers;
+    QList<double> mRecognizersWeights;
+    GesturesManager * mGesturesManager;
+    QList<Entity> mObjects;// Ideal gestures (name - path)
+    QString mLastGesture;
+    PathVector mGesture;
 };
