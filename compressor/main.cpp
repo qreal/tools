a3b07aa008af32b8a8146710c764f55d8db330e8
osechkina-masha <osechkina.masha@gmail.com>
2012-07-08 12:35:16 +0400

    fix minor bugs

--- compressor/main.cpp
+++ compressor/main.cpp
@@ -1,13 +1,12 @@
 #include <QtCore/QCoreApplication>
 #include "folderCompressor.h"
-#include "QDebug"
 
 
 int main(int argc, char *argv[])
 {
-	QString pack = argv[1];
-	QString source = argv[2];
-	QString result = argv[3];
+	QString const pack = argv[1];
+	QString const source = argv[2];
+	QString const result = argv[3];
 	QCoreApplication a(argc, argv);
 	if (pack == "-pack") {
 		FolderCompressor().compressFolder(source, result);
