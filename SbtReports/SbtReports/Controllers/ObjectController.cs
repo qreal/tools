7c3668a2dd508fe011c73d91d9da1b4488820b83
mosechkina <Maria.Osechkina@lanit-tercom.com>
2012-10-15 11:18:39 +0400

    delete some files some changes of transformator and integrator

--- SbtReports/SbtReports/Controllers/ObjectController.cs
+++ SbtReports/SbtReports/Controllers/ObjectController.cs
@@ -12,36 +12,19 @@ namespace SbtReports.Controllers
 {
   public class ObjectController : Controller
   {
-    private static List<DataObject> dataObjects = new List<DataObject>();
-    private static bool fillList = false;
+    //private static List<DataObject> dataObjects = new List<DataObject>();
     //
     // GET: /Object/
 
     public ViewResult Index()
     {
-      if (!fillList)
+      /*if (!fillList)
       {
         dataObjects = Transformator.ExecuteQuery();
         fillList = true;
       }
-      //Integrator.GenerateReport(dataObjects);
-      return View(dataObjects);
-    }
-
-    //
-    // GET: /Object/Details/5
-
-    public ViewResult Details(int id)
-    {
-      DataObject returnObject = new DataObject();
-      foreach (DataObject dataObject in dataObjects)
-      {
-        if (dataObject.ID == id)
-        {
-          returnObject = dataObject;
-        }
-      }
-      return View(returnObject);
+      Integrator.GenerateReport(dataObjects);*/
+      return View();
     }
 
     //
@@ -54,78 +37,16 @@ namespace SbtReports.Controllers
 
     //
     // POST: /Object/Create
-
+    //TODO::generate reports here!
     [HttpPost]
     public ActionResult Create(DataObject dataobject)
     {
       if (ModelState.IsValid)
       {
-        dataObjects.Add(dataobject);
+        Transformator.ExecuteQuery();
         return RedirectToAction("Index");
       }
       return View(dataobject);
-    }
-
-    //
-    // GET: /Object/Edit/5
-
-    public ActionResult Edit(int id)
-    {
-      DataObject returnObject = new DataObject();
-      foreach (DataObject dataObject in dataObjects)
-      {
-        if (dataObject.ID == id)
-        {
-          returnObject = dataObject;
-        }
-      }
-      return View(returnObject);
-    }
-
-    //
-    // POST: /Object/Edit/5
-
-    [HttpPost]
-    public ActionResult Edit(DataObject dataobject)
-    {
-      if (ModelState.IsValid)
-      {
-        foreach (DataObject dataObject in dataObjects)
-        {
-          if (dataobject.ID == dataObject.ID)
-          {
-            dataObjects.Remove(dataObject);
-            dataObjects.Add(dataobject);
-            return RedirectToAction("Index");
-          }
-        }
-      }
-      return View(dataobject);
-    }
-
-    //
-    // GET: /Object/Delete/5
-
-    public ActionResult Delete(int id)
-    {
-      return Edit(id);
-    }
-
-    //
-    // POST: /Object/Delete/5
-
-    [HttpPost, ActionName("Delete")]
-    public ActionResult DeleteConfirmed(int id)
-    {
-      foreach (DataObject dataObject in dataObjects)
-      {
-        if (dataObject.ID == id)
-        {
-          dataObjects.Remove(dataObject);
-          return RedirectToAction("Index");
-        }
-      }
-      return RedirectToAction("Index");
     }
 
     protected override void Dispose(bool disposing)
--- SbtReports/SbtReports/Integrator.cs
+++ SbtReports/SbtReports/Integrator.cs
@@ -10,52 +10,110 @@ namespace Sbt.General
 {
   public class Integrator
   {
+    private const string fileName = @"C:\Users\Masha\report.xlsx";
     public static void GenerateReport(List<DataObject> objects)
     {
-      const string fileName = @"C:\Users\Masha\report.xlsx";
-      using (SpreadsheetDocument doc = SpreadsheetDocument.Create(fileName, SpreadsheetDocumentType.Workbook))
+      SheetData data = new SheetData();
+
+      //add column names to the first row  
+      Row header = new Row();
+      header.RowIndex = (UInt32)1;
+
+      foreach (DataObject obj in objects)
       {
-        WorkbookPart workbook = doc.WorkbookPart;
-        WorksheetPart worksheet = workbook.WorksheetParts.Last();
-        SheetData data = worksheet.Worksheet.GetFirstChild<SheetData>();
-
-        //add column names to the first row  
-        Row header = new Row();
-        header.RowIndex = (UInt32)1;
-
-        Cell headerCell = createTextCell(1, 1, "header");
+        Cell headerCell = createTextCell(objects.IndexOf(obj) + 1, 1, obj.Name);
         header.AppendChild(headerCell);
-        data.AppendChild(header);
-
-        for (int i = 0; i < objects.Count; i++)
-        {
-          data.AppendChild(createContentRow(i + 2, objects[i].Name));
-        }
       }
+      data.AppendChild(header);
+      SpreadsheetDocument doc = CreateDoc(data);
+      /*using (SpreadsheetDocument spreadsheet = SpreadsheetDocument.Open(fileName, true))
+      {
+        WorkbookPart workbook = spreadsheet.WorkbookPart;
+        //create a reference to Sheet1  
+        WorksheetPart worksheet = workbook.WorksheetParts.Last();
+        
+        
+        ///loop through each data row  DataRow contentRow;
+        for (int i = 0; i < table.Rows.Count; i++)
+        {
+          contentRow = table.Rows[i];
+          data.AppendChild(createContentRow(contentRow, i + 2));
+        }
+       }  */          
     }
+    private static string getColumnName(int columnIndex)
+    {
+      int dividend = columnIndex;
+      string columnName = String.Empty;
+      int modifier;
 
-    private static Cell createTextCell(int columnIndex, int rowIndex, string cellValue)
+      while (dividend > 0)
+      {
+        modifier = (dividend - 1) % 26;
+        columnName = Convert.ToChar(65 + modifier).ToString() + columnName;
+        dividend = (int)((dividend - modifier) / 26);
+      }
+      return columnName;
+    }
+    private static Cell createTextCell(int columnIndex, int rowIndex, object cellValue)
     {
       Cell cell = new Cell();
 
       cell.DataType = CellValues.InlineString;
-      //cell.CellReference = getColumnName(columnIndex) + rowIndex;
+      cell.CellReference = getColumnName(columnIndex) + rowIndex;
 
       InlineString inlineString = new InlineString();
-      Text t = new Text(cellValue);
+      Text t = new Text();
+
+      t.Text = cellValue.ToString();
       inlineString.AppendChild(t);
       cell.AppendChild(inlineString);
 
       return cell;
     }
-    private static Row createContentRow(
-      int rowIndex, string text)
+    /*private Row createContentRow(
+    DataRow dataRow,
+    int rowIndex)
     {
-      Row row = new Row { RowIndex = (UInt32)rowIndex };
+      Row row = new Row
+      {
+        RowIndex = (UInt32)rowIndex
+      };
 
-      Cell dataCell = createTextCell(1, rowIndex, text);
-      row.AppendChild(dataCell);
+      for (int i = 0; i < dataRow.Table.Columns.Count; i++)
+      {
+        Cell dataCell = createTextCell(i + 1, rowIndex, dataRow[i]);
+        row.AppendChild(dataCell);
+      }
       return row;
+    }*/
+    private static SpreadsheetDocument CreateDoc(SheetData data)
+    {
+      using (SpreadsheetDocument doc = SpreadsheetDocument.Create(fileName, SpreadsheetDocumentType.Workbook))
+      {
+        doc.AddWorkbookPart();
+        doc.WorkbookPart.Workbook = new Workbook();
+        doc.WorkbookPart.Workbook.Save();
+        doc.WorkbookPart.Workbook.Sheets = new Sheets();
+        doc.WorkbookPart.Workbook.Save();
+        Sheets sheets = doc.WorkbookPart.Workbook.GetFirstChild<Sheets>();
+
+        // Add the worksheetpart
+        WorksheetPart worksheetPart = doc.WorkbookPart.AddNewPart<WorksheetPart>();
+        worksheetPart.Worksheet = new Worksheet(data);
+        worksheetPart.Worksheet.Save();
+
+        // Add the sheet and make relation to workbook
+        Sheet sheet = new DocumentFormat.OpenXml.Spreadsheet.Sheet()
+        {
+          Id = doc.WorkbookPart.GetIdOfPart(worksheetPart),
+          SheetId = (uint)(doc.WorkbookPart.Workbook.Sheets.Count() + 1),
+          Name = "sheet"
+        };
+        sheets.Append(sheet);
+        doc.WorkbookPart.Workbook.Save();
+        return doc;
+      }
     }
   }
 }
