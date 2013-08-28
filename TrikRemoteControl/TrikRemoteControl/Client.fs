cec93900ec7715ffa0534042121a6be7718e2336
Yurii Litvinov <yurii.litvinov@gmail.com>
2013-08-28 17:24:54 +0400

    refactored client

--- TrikRemoteControl/TrikRemoteControl/Client.fs
+++ TrikRemoteControl/TrikRemoteControl/Client.fs
@@ -1,4 +1,5 @@
 namespace TrikRemoteControl
+open System
 
 module Constants =
     [<Literal>]
@@ -6,76 +7,56 @@ module Constants =
     [<Literal>]
     let SendTimeout = 2000
 
-type ConnectionState =
-    abstract Send : string -> unit
-    abstract DisconnectedEvent : IEvent<unit>
-
-type ConnectedState (client : System.Net.Sockets.TcpClient, syncContext : System.Threading.SynchronizationContext) as this =
-
-    let disconnectedEvent = Event<_> ()
-
-    let keepAliveTimer = new System.Timers.Timer (1000.0, AutoReset = true)
-    do keepAliveTimer.Elapsed.Add (fun _ -> (this :> ConnectionState).Send "keepalive")
-    do keepAliveTimer.Start ()
-
-    /// Network data stream.
-    let stream = client.GetStream ()
-
-    /// Writer for network stream.
-    let writer = new System.IO.StreamWriter (stream, AutoFlush = true)
-
-    interface ConnectionState with
-        member x.Send (str : string) = 
-            let postEvent (event : Event<_>) = syncContext.Post (System.Threading.SendOrPostCallback (fun _ -> event.Trigger ()), null)
-            async {
-                do! Async.AwaitIAsyncResult (writer.WriteLineAsync str, Constants.SendTimeout) |> Async.Ignore
-                if not client.Connected then
-                    postEvent disconnectedEvent
-                    
-            } |> Async.Start
-
-        member this.DisconnectedEvent = disconnectedEvent.Publish
-
-type DisconnectedState () =
-    let disconnectedEvent = Event<_> ()
-
-    interface ConnectionState with
-        member x.Send (str : string) = ()
-        member this.DisconnectedEvent = disconnectedEvent.Publish
-
 /// Network client. Can connect to some endpoint, send and receive string data.
-type Client (host : string, port) =
+type Client (host : string, port: int) =
 
     /// TCP client that is used for communication.
-    let client = new System.Net.Sockets.TcpClient ()
-    let mutable connectionState : ConnectionState = DisconnectedState () :> _
+    let client = new System.Net.Sockets.TcpClient()
+    let mutable connectionState = None
     let syncContext = System.Threading.SynchronizationContext.Current
-
+    
     let connectedEvent = Event<_> ()
     let connectionFailedEvent = Event<_> ()
-    let disconnectedEvent = Event<_> ()
+    let disconnectedEvent = Event<_> ()    
+    let postEvent (event : Event<_>) = syncContext.Post (System.Threading.SendOrPostCallback (fun _ -> event.Trigger ()), null)
 
     member this.ConnectedEvent = connectedEvent.Publish
     member this.ConnectionFailedEvent = connectionFailedEvent.Publish
     member this.DisconnectedEvent = disconnectedEvent.Publish
-
-    member this.Connect () =
-        let postEvent (event : Event<_>) = syncContext.Post (System.Threading.SendOrPostCallback (fun _ -> event.Trigger ()), null)
-
+    
+    member this.Connect () =        
+        this.Disconnect()
         async {
-            let! result = Async.AwaitIAsyncResult (client.ConnectAsync(host, port), Constants.ConnectionTimeout)
-            if result && client.Connected then
+            let! _ = Async.AwaitIAsyncResult (client.ConnectAsync(host, port), Constants.ConnectionTimeout)
+            if client.Connected then
                 postEvent connectedEvent
-                connectionState <- ConnectedState (client, syncContext) :> ConnectionState
-                connectionState.DisconnectedEvent.Add (fun () -> disconnectedEvent.Trigger ())
+                let keepAliveTimer = new System.Timers.Timer (1000.0, AutoReset = true)
+                keepAliveTimer.Elapsed.Add (fun _ -> this.Send "keepalive")
+                keepAliveTimer.Start ()
+                connectionState <-  Some (new System.IO.StreamWriter (client.GetStream (), AutoFlush = true), keepAliveTimer)                
             else
                 postEvent connectionFailedEvent
         } |> Async.Start
 
-    /// Send given string to a server.
-    member this.Send (str : string) = connectionState.Send str
-
-    member this.Dispose () = (client :> System.IDisposable).Dispose ()
+    member this.Send (str : string) = 
+           connectionState |> Option.iter (fun (writer, timer) ->
+               async {
+                   do! Async.AwaitIAsyncResult (writer.WriteLineAsync str, Constants.SendTimeout) |> Async.Ignore
+                   if not client.Connected then
+                       this.Disconnect()
+                } |> Async.Start)
+                    
+    member this.Disconnect () = 
+        match connectionState with
+          | None -> ()
+          | Some (writer, timer) -> 
+              writer.Close()
+              timer.Dispose()
+              connectionState <- None
+              postEvent disconnectedEvent
+              
+        
+    member this.Dispose () = this.Disconnect(); (client :> System.IDisposable).Dispose ()
 
     interface System.IDisposable with
         member this.Dispose () = this.Dispose ()
