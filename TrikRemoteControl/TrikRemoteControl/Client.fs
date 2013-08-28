444d0a1afc89c34bc30eedc9da26e6e31b2e4e35
yurii-litvinov <yurii.litvinov@gmail.com>
2013-08-28 12:50:37 +0400

    - reworked "codebehind" to be always in consistent state - asynchronous network operations

--- TrikRemoteControl/TrikRemoteControl/Client.fs
+++ TrikRemoteControl/TrikRemoteControl/Client.fs
@@ -1,11 +1,13 @@
 namespace TrikRemoteControl
 
-/// Network client. Can connect to some endpoint, send and receive string data.
-type Client (host, port) =
-    
-    /// TCP client that is used for communication.
-    let client = new System.Net.Sockets.TcpClient (host, port)
+module Constants =
+    let connectionTimeout = 2000
+    let sendTimeout = 2000
 
+type ConnectionState =
+    abstract Send : string -> unit
+
+type ConnectedState (client : System.Net.Sockets.TcpClient) =
     /// Network data stream.
     let stream = client.GetStream ()
 
@@ -13,13 +15,42 @@ type Client (host, port) =
     let writer = new System.IO.StreamWriter (stream)
     do writer.AutoFlush <- true
 
-    /// Loop that waits for connection to a server to be opened.
-    let rec waitForConnection () =
-        if not client.Connected then 
-            waitForConnection ()
-        else
-            System.Threading.Thread.Sleep 100
-    do waitForConnection ()
+    interface ConnectionState with
+        member x.Send (str : string) = 
+            async {
+                do! Async.AwaitIAsyncResult (writer.WriteLineAsync str, Constants.sendTimeout) |> Async.Ignore
+            } |> Async.Start
+
+type DisconnectedState () =
+    interface ConnectionState with
+        member x.Send (str : string) = ()
+
+/// Network client. Can connect to some endpoint, send and receive string data.
+type Client (host : string, port) =
+    
+    /// TCP client that is used for communication.
+    let client = new System.Net.Sockets.TcpClient ()
+    let mutable connectionState : ConnectionState = DisconnectedState () :> _
+
+    let connectedEvent = Event<_> ()
+    let connectionFailedEvent = Event<_> ()
+
+    member x.ConnectedEvent = connectedEvent.Publish
+    member x.ConnectionFailedEvent = connectionFailedEvent.Publish
+
+    member x.Connect () =
+        let syncContext = System.Threading.SynchronizationContext.Current
+        let postEvent (event : Event<_>) = syncContext.Post (System.Threading.SendOrPostCallback (fun _ -> event.Trigger ()), null)
+
+        async {
+            let! result = Async.AwaitIAsyncResult (client.ConnectAsync(host, port), Constants.connectionTimeout)
+            if result then
+                postEvent connectedEvent
+                connectionState <- ConnectedState client :> ConnectionState
+            else
+                postEvent connectionFailedEvent
+        } |> Async.Start
 
     /// Send given string to a server.
-    member this.Send (str : string) = writer.WriteLine str
+    member this.Send (str : string) = connectionState.Send str
+
--- TrikRemoteControl/TrikRemoteControl/Main.fs
+++ TrikRemoteControl/TrikRemoteControl/Main.fs
@@ -7,50 +7,63 @@ open TrikRemoteControl
 
 let port = 8888
 
-type MainWindow () =
+type MainWindow (mainWindow : Window) =
+
+    let button name = mainWindow.FindName name :?> Button
+
+    let ipTextBox = mainWindow.FindName "ipTextBox" :?> TextBox
+    let connectButton = button "connect"
+    let upButton = button "up"
+    let downButton = button "down"
+    let leftButton = button "left"
+    let rightButton = button "right"
+    let stopButton = button "stop"
+    let buttons = [upButton; downButton; leftButton; rightButton; stopButton]
 
     let mutable client : Client option = None
-    let mutable controlsRegistered = false
+
+    let setButtonsEnabled enabled = for button in buttons do button.IsEnabled <- enabled
 
     let connect ip =
         if client.IsNone then
             client <- Some <| Client (ip, port)
+            client.Value.ConnectedEvent |> Event.add (fun _ -> setButtonsEnabled true)
+            client.Value.ConnectionFailedEvent |> Event.add (fun _ -> client <- None)
+            client.Value.Connect ()
 
     let sendCommand command =
         if client.IsSome then
             client.Value.Send command
 
-    member x.RegisterControls (mainWindow : Window) =
-        if not controlsRegistered then
+    let init () =
+        let registerButtonHandler buttonName handler =
+            let button = mainWindow.FindName buttonName :?> Button
+            Event.add handler button.Click
 
-            let registerButtonHandler buttonName handler =
-                let button = mainWindow.FindName buttonName :?> Button
-                Event.add handler button.Click
+        let registerCommand button =
+            let commandScriptFileName = "scripts/" + button + ".qts"
+            registerButtonHandler button (fun _ -> sendCommand <| System.IO.File.ReadAllText commandScriptFileName)
 
-            let ipTextBox = mainWindow.FindName "ipTextBox" :?> TextBox
+        registerButtonHandler "connect" (fun _ -> connect <| ipTextBox.Text)
 
-            registerButtonHandler "connect" (fun _ -> connect <| ipTextBox.Text)
+        registerCommand "up"
+        registerCommand "down"
+        registerCommand "left"
+        registerCommand "right"
+        registerCommand "stop"
 
-            let registerCommand button =
-                let commandScriptFileName = "scripts/" + button + ".qts"
-                registerButtonHandler button (fun _ -> sendCommand <| System.IO.File.ReadAllText commandScriptFileName)
+        setButtonsEnabled false
 
-            registerCommand "up"
-            registerCommand "down"
-            registerCommand "left"
-            registerCommand "right"
-            registerCommand "stop"
-
-            controlsRegistered <- true
+    do init ()
 
 [<STAThread>]
 [<EntryPoint>]
 let main _ = 
     let application = Application.LoadComponent (new Uri ("App.xaml", UriKind.Relative)) :?> Application
 
-    let mainWindow = MainWindow () 
+    let mainWindow = ref None
 
     application.Activated
-    |> Event.add (fun _ -> mainWindow.RegisterControls (application.MainWindow))
+    |> Event.add (fun _ -> if (!mainWindow).IsNone then mainWindow := Some <| MainWindow(application.MainWindow))
 
     application.Run ()
